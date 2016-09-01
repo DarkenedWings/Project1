a59578e5e7779e3067261e11da71533d5d352c9d
iRsonHar <iRsonHar@HARRISON-PC>
2016-09-01 19:57:36 -0400

    Add project files.

---
+++ CardGames/Deck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGames
+{
+    class Deck
+    {
+        int[] Cards = new int[52];
+
+        public void CardGames()
+        {
+            for (int i = 0; i < Cards.Length/4; i++)
+            {
+                Cards[i] = i + 1;
+            }
+        }
+    }
+}
